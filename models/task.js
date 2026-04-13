996dde05f365ff8bcc5d9888cde36b7365c62b5e
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-04-14 00:16:46 +0800

    feat: connect to mongodb, create schema, and export mongoose model

---
+++ models/task.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+console.log(`Connecting to ${url}`)
+mongoose.connect(url, { family: 4 })
+  .then(() => {
+    console.log('Successfully connected to MongoDB')
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
+
+const taskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    minLength: 3,
+    maxLength: 25,
+    required: true
+  }
+})
+
+taskSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Task', taskSchema)
