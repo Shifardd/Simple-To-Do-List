10f60d37a068827f54afaf0f957e8028470ffaae
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-04-14 00:20:16 +0800

    feat: add requestLogger and errorHandler, and update the request functions to save and display data from database

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
+require('dotenv').config()
+const Task = require('./models/task')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(morgan('tiny'))
 
-let toDo = [
-  {
-    task: 'Clean',
-    id: '1'
-  },
-  {
-    task: 'Study',
-    id: '2'
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome!</h1>')
 })
 
 app.get('/api/task', (req, res) => {
-  res.json(toDo)
+  Task.find({})
+    .then(tasks => {
+      res.json(tasks)
+    })
 })
 
-app.get('/api/task/:id', (req, res) => {
-  const id = req.params.id
-  const task = toDo.find(task => task.id === id)
-  if(!task) {
-    res.status(404).send({ error: 'malformed id' })
-  }
-  res.json(task)
+app.get('/api/task/:id', (req, res, next) => {
+  Task.findById(req.params.id)
+    .then(specificTask => {
+      if(specificTask) {
+        res.json(specificTask)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  return String(Math.round(Math.random() * 100000))
-}
 
-app.post('/api/task', (req, res) => {
+app.post('/api/task', (req, res, next) => {
   const taskObject = req.body
 
   if(!taskObject.task) {
     return res.status(400).send({ error: 'Invalid input' })
   }
 
-  const newTask = {
-    task: taskObject.task,
-    id: generateId()
+  const newTask = new Task({
+    task: taskObject.task
+  })
+
+  newTask.save()
+    .then(task => {
+      res.json(task)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/task/:id', (req, res, next) => {
+  Task.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if(error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  } else if (error.name === 'CastError') {
+    return res.status(404).json({ error: 'malformed id' })
   }
+  next(error)
+}
 
-  toDo = toDo.concat(newTask)
-  res.json(newTask)
-})
+app.use(errorHandler)
 
-app.delete('/api/task/:id', (req, res) => {
-  const id = req.params.id
-  toDo = toDo.filter(task => task.id !== id)
-  res.status(204).end()
-})
-
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`)
 })
